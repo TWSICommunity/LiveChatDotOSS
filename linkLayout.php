f3189b28eab2f626e79214f7e5ab55203b3af0f6
TWSICommunity <gamewolf10@gmail.com>
2018-04-02 12:23:22 -0700

    Update linkLayout.php

--- linkLayout.php
+++ linkLayout.php
@@ -1,1 +1,1 @@
-<a href="index.html">Home</a> >> <a href="enter.html">Enter Livechat</a> >> <a href="faq.html"> >> <a href="rules.html">Read the rules</a>
+echo '<a href="index.html">Home</a> >> <a href="enter.html">Enter Livechat</a> >> <a href="faq.html"> >> <a href="rules.html">Read the rules</a>';
